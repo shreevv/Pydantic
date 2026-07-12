41e90cf81aa8f41a2c7aacf2ca5fba562e9a9ecd
Shree Dutt Pandey <sduttmay4@gmail.com>
2026-07-12 11:43:29 +0530

    #creating a base model

--- 01.py
+++ 01.py
@@ -1,10 +1,8 @@
 #Creating a Base Model
 from pydantic import BaseModel
-
 class Patient(BaseModel):
     name :str
     age: int
-# Storing Info in Dictionary
 patient_info={'name':'Shree','age':20}
 #Unpacking the dictionary
 patient1=Patient(**patient_info)
