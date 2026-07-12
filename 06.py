b90509eaa302590f4be1d605d16c3f8cd76ff1fd
Shree Dutt Pandey <sduttmay4@gmail.com>
2026-07-12 11:45:12 +0530

    Nested Models

---
+++ 06.py
@@ -0,0 +1,19 @@
+#Nested Models
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city:str
+    state:str
+    pin:str
+
+class Patient(BaseModel):
+    name:str
+    gender:str
+    age:int
+    address: Address
+
+address_dict={'city':'gurgaon','state':'haryana','pin':'122001'}
+address1=Address(**address_dict)
+patient_dict={'name':'shree','gender':'male','age':35,'address':address1}
+patient1= Patient(**patient_dict)
+print(patient1)
